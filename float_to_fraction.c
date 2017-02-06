96bad3de8809325caadfe4626188aa092f791350
pam-phy <pam.phys.th@gamil.com>
2017-02-06 15:26:39 +0800

    cgc

--- float_to_fraction.c
+++ float_to_fraction.c
@@ -1,38 +1,46 @@
 #include <stdio.h>
 #include <math.h>
-#define TOP 10000000000
+#define TOP 100000
+
+void float_to_frac(double , int);
 
 int main(void)
 {
-	double input, test;
-	int nume, deno;
-	int check;
-	int m, n;
+	double input;
 
 	printf("Enter a floating-point number: ");
 	scanf("%lf", &input);
 
-	for (m = 1; m < TOP; m++)
+	float_to_frac(input, TOP);
+
+	return 0;
+}
+
+void float_to_frac(double input, int top)
+{
+	int m, n;
+	int nume, deno;
+	int value;
+	int check;
+
+	for (m = 1; m < top; m++)
 	{
 		check = 0;
-		test = input * m;
-		for (n = 1; n < m; n++)
+		value = input * m;
+		for (n = 0; n < m; n++)
 		{
-			if (fabs(test - n) < (1.0 / TOP))
+			if (fabs(value - n) < (1.0 / top))
 			{
 				check = 1;
-				nume = n;	// numerator
+				nume = n;
 				break;
 			}
 		}
 		if (1 == check)
 		{
-			deno = m;		// denominator
+			deno = m;
 			break;
 		}
 	}
-
-	printf("%.10f = %d/%d\n", input, n, m);
-
-	return 0;
+	printf("%.10f = %d/%d\n", input, nume, deno);
 }
