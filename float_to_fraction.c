09ad6009bdfbfa5bfc7b35cd5a6f28b4379b6621
pam-phy <pam.phys.th@gamil.com>
2017-02-06 15:01:44 +0800

    cgc

---
+++ float_to_fraction.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <math.h>
+#define TOP 100000
+
+int main(void)
+{
+	double input, test;
+	int nume, deno;
+	int check;
+	int m, n;
+
+	printf("Enter a floating-point number: ");
+	scanf("%lf", &input);
+
+	for (m = 1; m < TOP; m++)
+	{
+		check = 0;
+		test = input * m;
+		for (n = 1; n < m; n++)
+		{
+			if (fabs(test - n) < 0.000001)
+			{
+				check = 1;
+				nume = n;
+				break;
+			}
+		}
+		if (1 == check)
+		{
+			deno = m;
+			break;
+		}
+	}
+
+	printf("%f = %d/%d\n", input, n, m);
+
+	return 0;
+}
